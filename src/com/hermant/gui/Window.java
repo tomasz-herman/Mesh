1b034d5fcda6e3ce08058ae4bd1981f162f93c23
tomasz-herman <tomasz.herman@protonmail.com>
2020-02-13 13:42:53 +0100

    fullscreen mode

--- src/com/hermant/gui/Window.java
+++ src/com/hermant/gui/Window.java
@@ -2,14 +2,20 @@ package com.hermant.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Window extends JFrame {
+public class Window extends JFrame implements KeyListener {
 
     private static final int MINIMUM_WIDTH = 320;
     private static final int MINIMUM_HEIGHT = 240;
+    private static final GraphicsDevice DEVICE =
+            GraphicsEnvironment.getLocalGraphicsEnvironment()
+                    .getScreenDevices()[0];
 
     private Engine engine;
     private String title;
+    private boolean fullscreen = false;
 
     public Window(String title, int width, int height){
         this.title = title;
@@ -29,6 +35,7 @@ public class Window extends JFrame {
         addMouseMotionListener(engine);
         addKeyListener(engine);
         addMouseWheelListener(engine);
+        addKeyListener(this);
     }
 
     public void setText(String text){
@@ -41,5 +48,42 @@ public class Window extends JFrame {
 
     public void start(){
         engine.start();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+
+    private int lastWidth, lastHeight;
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+            System.exit(0);
+        }
+        if(e.getKeyCode() == KeyEvent.VK_F){
+            dispose();
+            if(fullscreen){
+                DEVICE.setFullScreenWindow(null);
+                setExtendedState(NORMAL);
+                setUndecorated(false);
+                setSize(lastWidth, lastHeight);
+                setLocationRelativeTo(null);
+                setVisible(true);
+            } else {
+                lastWidth = getWidth();
+                lastHeight = getHeight();
+                setUndecorated(true);
+                DEVICE.setFullScreenWindow(this);
+                setVisible(true);
+            }
+            fullscreen = !fullscreen;
+        }
     }
 }
